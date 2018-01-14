bef1b29cd4c8da822481ff312dffbdd95f07e656
waderoberts123 <32141163+waderoberts123@users.noreply.github.com>
2018-01-14 00:11:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name = 'hydrostats',
   packages = ['hydrostats'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.31',
   description = 'Error metrics for use in comparison studies, specifically for use in the field of hydrology',
   author = 'Wade Roberts',
   author_email = 'waderoberts123@gmail.com',
@@ -12,6 +12,6 @@ setup(
   download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['hydrology', 'error', 'metrics', 'comparison', 'statistics'], # arbitrary keywords
   classifiers = ["License :: OSI Approved :: MIT License",
-                 "Programming Language :: Python :: 3.4",
+                 "Programming Language :: Python :: 3.6",
 ]
 )
