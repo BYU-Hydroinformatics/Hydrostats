41353c11be1b5b46667e4f098173970979692afa
waderoberts123 <32141163+waderoberts123@users.noreply.github.com>
2018-01-13 17:30:48 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(
+  name = 'hydrostats',
+  packages = ['hydrostats'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Error metrics for use in comparison studies, specifically for use in the field of hydrology',
+  author = 'Wade Roberts',
+  author_email = 'waderoberts123@gmail.com',
+  url = 'https://github.com/waderoberts123/hydrostats', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['hydrology', 'error', 'metrics', 'comparison', 'statistics'], # arbitrary keywords
+  classifiers = ["License :: OSI Approved :: MIT License",
+                 "Programming Language :: Python :: 3.4",
+]
+)
