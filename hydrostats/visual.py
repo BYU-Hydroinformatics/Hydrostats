aff67e9e104512d5412843ab3dc3d0cc066f87d4
waderoberts123 <32141163+waderoberts123@users.noreply.github.com>
2018-02-02 08:17:29 -0700

    Update visual.py

--- hydrostats/visual.py
+++ hydrostats/visual.py
@@ -1,6 +1,10 @@
 from hydrostats import *
-import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import numpy as np
+import matplotlib.pyplot as plt
+import hydrostats.data as hd
+import sympy as sp
+import scipy.stats as stats
 
 
 def plot(merged_data_df, legend=None, metrics=None, grid=False, title=None, force_x=None, labels=None):
@@ -67,3 +71,94 @@ def plot(merged_data_df, legend=None, metrics=None, grid=False, title=None, forc
         plt.text(-0.35, 0.75, formatted_selected_metrics, ha='left', va='center', transform=ax.transAxes, fontdict=font)
         plt.subplots_adjust(left=0.25)
     plt.show()
+
+    
+def hist(merged_data_df, legend, bins, grid=False, title=None, labels=None, savefigure=None):
+    """Bins needs to be a numpy array"""
+    fig, ax1 = plt.subplots(figsize=(12, 7))
+    sim = merged_data_df.iloc[:, 0].as_matrix()
+    obs = merged_data_df.iloc[:, 1].as_matrix()
+    ax1.hist(sim, bins, alpha=0.5, label=legend[0], edgecolor='black', linewidth=0.5)
+    ax1.hist(obs, bins, alpha=0.5, label=legend[1], edgecolor='black', linewidth=0.5)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    if grid:
+        plt.grid(True)
+    if title:
+        plt.title(title, fontsize=20)
+    if labels:
+        plt.xlabel(labels[0], fontsize=18)
+        plt.ylabel(labels[1], fontsize=18)
+    ax1.legend(loc='upper right', framealpha=1)
+    plt.tight_layout()
+    if savefigure:
+        plt.savefig(savefigure)
+    else:
+        plt.show()
+
+      
+def scatter(merged_data_df, grid=False, title=None, labels=None, best_fit=None, savefigure=None):
+    plt.figure(figsize=(12, 7))
+    sim = merged_data_df.iloc[:, 0].as_matrix()
+    obs = merged_data_df.iloc[:, 1].as_matrix()
+    plt.plot(sim, obs, 'ko')
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    if grid:
+        plt.grid(True)
+    if title:
+        plt.title(title, fontsize=20)
+    if labels:
+        plt.xlabel(labels[0], fontsize=18)
+        plt.ylabel(labels[1], fontsize=18)
+    if best_fit:
+        # Getting a polynomial fit and defining a function with it
+        p = np.polyfit(sim, obs, 1)
+        f = np.poly1d(p)
+
+        # calculate new x's and y's
+        x_new = np.linspace(0, sim.max(), sim.size)
+        y_new = f(x_new)
+
+        # Formatting the best fit equation to be able to display in latex
+        x = sp.symbols("x")
+        poly = sum(sp.S("{:6.4f}".format(v)) * x ** i for i, v in enumerate(p[::-1]))
+        eq_latex = sp.printing.latex(poly)
+
+        # Plotting the best fit line with the equation as a legend in latex
+        plt.plot(x_new, y_new, 'k', label="${}$".format(eq_latex))
+        plt.legend(fontsize=12)
+    if savefigure:
+        plt.savefig(savefigure)
+    else:
+        plt.show()
+        
+        
+def qq_plot(merged_data_df, grid=False, title=None, labels=None, savefigure=None, rvalue=None):
+    fig = plt.figure(figsize=(12, 7))
+    ax = fig.add_subplot(111)
+    measurements = merged_data_df.iloc[:, 0] - merged_data_df.iloc[:, 1]
+    (osm, osr), (slope, intercept, r) = stats.probplot(measurements, dist="norm")
+    print(osm, osr, slope, intercept, r)
+    plt.plot(osm, osr, 'bo', osm, slope * osm + intercept, 'r-')
+    if rvalue:
+        xmin = np.min(osm)
+        xmax = np.max(osm)
+        ymin = np.min(measurements)
+        ymax = np.max(measurements)
+        posx = xmin + 0.70 * (xmax - xmin)
+        posy = ymin + 0.01 * (ymax - ymin)
+        plt.text(posx, posy, "$R^2=%1.4f$" % r**2)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    if grid:
+        ax.grid(True)
+    if title:
+        ax.set_title(title, fontsize=20)
+    if labels:
+        ax.set_xlabel(labels[0], fontsize=18)
+        ax.set_ylabel(labels[1], fontsize=18)
+    if savefigure:
+        plt.savefig(savefigure)
+    else:
+        plt.show()
