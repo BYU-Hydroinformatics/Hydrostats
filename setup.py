34c8cad9697b0a4b5f82ae290c813e085f733db4
waderoberts123 <32141163+waderoberts123@users.noreply.github.com>
2018-02-03 16:13:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name = 'hydrostats',
   packages = ['hydrostats'], # this must be the same as the name above
-  version = '0.38',
+  version = '0.39',
   description = 'Error metrics for use in comparison studies, specifically for use in the field of hydrology',
   author = 'Wade Roberts',
   author_email = 'waderoberts123@gmail.com',
@@ -16,7 +16,7 @@ setup(
                  ],
   install_requires=[
           'numpy',
-          'numexpr',
+          'numba',
           'pandas',
           'matplotlib',
           'scipy',
