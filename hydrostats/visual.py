54627bdb288be2f4daae35c24c761b35073242e3
waderoberts123 <32141163+waderoberts123@users.noreply.github.com>
2018-03-16 13:45:25 -0600

    Plot Function
    Changed the plot function to have new arguments and able to loop with savefig argument.

--- hydrostats/visual.py
+++ hydrostats/visual.py
@@ -1,53 +1,71 @@
-from hydrostats import *
-import matplotlib.patches as mpatches
+from hydrostats import me, mae, mse, ed, ned, rmse, rmsle, mase, r_squared, acc, mape, mapd, smap1, smap2, d, d1, dr, \
+    drel, dmod, M, R, NSE, NSEmod, NSErel, E_1, sa, sc, sid, sga
 import numpy as np
 import matplotlib.pyplot as plt
-import hydrostats.data as hd
 import sympy as sp
 import scipy.stats as stats
+import calendar
 
 
-def plot(merged_data_df, legend=None, metrics=None, grid=False, title=None, force_x=None, labels=None, savefigure=None):
-    """Daily means you want to plot daily values, in which we need to space the x ticks
-    legend - put a list of the two legend values [sim, obs]
-    metrics - include the metrics you want placed on the graph
-    grid - boolean
-    title - string indicating the title
-    force_x - number that indicates if you want to force x values every nth time series, only use when x axis is cramped
-    labels - x and y axis labels, have a string list ["x", 'y']"""
+def plot(merged_data_df, legend=None, metrics=None, grid=False, title=None, x_season=False, labels=None,
+         savefigure=None, linestyles=['ro', 'b^'], tight_xlim=False):
+    """A function that will plot a time series and
+    Arguments:
+    merged_data_df - A pandas dataframe with a datetime type index, and two columns of simulated and observed data of
+    float type, respectively.
+    legend - A list of two string type inputs specifying the two legend text values (e.g. ['sim', 'obs']).
+    metrics - A list of string type input indicating the metrics you want placed to the left of the graph
+    (See documentation for a full list of the available metrics).
+    grid - Boolean type indicating whether a graph is desired
+    title - String type input indicating the title of the plot.
+    x_season - For use when a dataframe has been converted to monthly or daily averages or standard error with a
+    hydrostats.data function. A boolean type argument that will change the string values to monthly values.
+    labels - A list of string type inputs specifying the x and y axis labels, respectively.
+    savefigure - A string type input requiring the path and the filename where the figure will be save instead of
+    displayed (e.g. r'path/to/file/image.png)
+    linestyles - A list of two string type inputs specifying the linestyles (see documentation for the available
+    options.
+    tight_xlim - A boolean type input indicating if the user would like the gap in the x limits of the graph to be
+    removed."""
     fig = plt.figure(num=1, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
     ax = fig.add_subplot(111)
-    plt.plot(merged_data_df.index, merged_data_df.iloc[:, 0], 'ro', markersize=2)
-    plt.plot(merged_data_df.index, merged_data_df.iloc[:, 1], 'b^', markersize=2)
-    if force_x:
-        # Get the current axis
-        ax = plt.gca()
-        # Only label every 20th value
-        ticks_to_use = merged_data_df.index[::force_x]
-        # Now set the ticks and labels
-        ax.set_xticks(ticks_to_use)
-        ax.set_xticklabels(ticks_to_use)
-        plt.xticks(fontsize=14, rotation=45)
+    plt.plot(merged_data_df.index, merged_data_df.iloc[:, 0], linestyles[0], markersize=2,
+             label=legend[0])
+    plt.plot(merged_data_df.index, merged_data_df.iloc[:, 1], linestyles[1], markersize=2,
+             label=legend[1])
+    if tight_xlim:
+        plt.xlim(merged_data_df.index[0], merged_data_df.index[-1])
+    if x_season:
+        seasons = calendar.month_abbr[1:13]
+        day_month = np.array([31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+        day_month_cum = np.cumsum(day_month)
+        fractions = day_month_cum[:11] / 366
+        fractions = np.insert(fractions, 0, 0)
+        index = np.rint(fractions * len(merged_data_df.index)).astype(np.integer)
+        plt.xticks(merged_data_df.index[index], seasons, fontsize=14, rotation=45)
     else:
         plt.xticks(fontsize=14, rotation=45)
     plt.yticks(fontsize=14)
     if labels:
-        #Plotting Labels
+        # Plotting Labels
         plt.xlabel(labels[0], fontsize=18)
         plt.ylabel(labels[1], fontsize=18)
     if title:
-        plt.title(title, fontsize=20)
+        title_dict = {'family': 'sans-serif',
+                      'color': 'black',
+                      'weight': 'normal',
+                      'size': 20,
+                      }
+        ax.set_title(label=title, fontdict=title_dict, pad=25)
     if legend:
-        sim = mpatches.Patch(color='red', label=legend[0])
-        obs = mpatches.Patch(color='blue', label=legend[1])
-        plt.legend(handles=[sim, obs])
+        plt.legend()
     if grid:
         plt.grid(True)
     if metrics:
         forecasted_array = merged_data_df.iloc[:, 0].as_matrix()
         observed_array = merged_data_df.iloc[:, 1].as_matrix()
         function_list = [me, mae, mse, ed, ned, rmse, rmsle, mase, r_squared, acc, mape, mapd, smap1, smap2, d, d1, dr,
-                         drel, dmod, M, R, E, Emod, Erel, E_1, sa, sc, sid, sga
+                         drel, dmod, M, R, NSE, NSEmod, NSErel, E_1, sa, sc, sid, sga
                          ]
         function_list_str = ['ME', 'MAE', 'MSE', 'ED', 'NED', 'RMSE', 'RMSLE', 'MASE', 'R^2', 'ACC', 'MAPE',
                              'MAPD', 'SMAP1', 'SMAP2', 'D', 'D1', 'DR', 'D-Rel', 'D-Mod', 'M', 'R', 'E', 'E-Mod',
@@ -72,6 +90,7 @@ def plot(merged_data_df, legend=None, metrics=None, grid=False, title=None, forc
         plt.subplots_adjust(left=0.25)
     if savefigure:
         plt.savefig(savefigure)
+        plt.close()
     else:
         plt.show()
 
