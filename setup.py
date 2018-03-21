7c84c36d412147a9f35f60962c3937aee2ccc573
waderoberts123 <32141163+waderoberts123@users.noreply.github.com>
2018-03-20 20:18:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 setup(
     name='hydrostats',
     packages=['hydrostats'],  # this must be the same as the name above
-    version='0.43',
+    version='0.45',
     description='Error metrics for use in comparison studies, specifically for use in the field of hydrology',
     author='Wade Roberts',
     author_email='waderoberts123@gmail.com',
     url='https://github.com/waderoberts123/hydrostats',  # use the URL to the github repo
-    download_url='https://github.com/waderoberts123/Hydrostats/archive/4.3.tar.gz',
+    download_url='https://github.com/waderoberts123/Hydrostats/archive/4.5.tar.gz',
     keywords=['hydrology', 'error', 'metrics', 'comparison', 'statistics', 'forecast', 'observed'],
     # arbitrary keywords
     classifiers=["License :: OSI Approved :: MIT License",
@@ -23,5 +23,6 @@ setup(
         'matplotlib >= 2.2.0',
         'scipy >= 1.0.0',
         'sympy >= 1.1.1',
+        'statsmodels'
     ]
 )
