127da1e2640b15ec2a4106f0087df4b645ad5c30
Wade Roberts <waderoberts123@gmail.com>
2018-07-26 11:10:38 -0700

    Update Docs

--- docs/latex_for_all_metrics.py
+++ docs/latex_for_all_metrics.py
@@ -1,7 +1,7 @@
 import sympy as sp
 
 latex_symbols = {
-    'me': r'$$\int_0^1 e^x\,dx$$',
+    'me': r'$$ME = \frac{1}{n} \sum_{i=0}^{n} (Sim_i - Obs_i)$$',
 }
 
-sp.preview(latex_symbols['me'])
+sp.preview(latex_symbols['me'], viewer='file', filename='test.png')
