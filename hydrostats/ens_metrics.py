e40acc424e4a9bbf0d7d0045d576f8214681f1c6
waderoberts123 <waderoberts123@gmail.com>
2018-08-07 13:20:39 -0600

    Updates to Ensemble Metrics

--- hydrostats/ens_metrics.py
+++ hydrostats/ens_metrics.py
@@ -13,13 +13,13 @@ from __future__ import division
 import numpy as np
 import warnings
 
-__all__ = ["ens_me", "crpsHersbach", "crpsKernel"]
+__all__ = ["ens_me", "crps_hersbach", "crps_kernel"]
 
 # TODO: Should there be an error instead of a warning if the observed or forecast values are all 0?
 
 
 def ens_me(obs, fcst_ens=None, remove_zero=False, remove_neg=False):
-    """Calculate the mean error between observed values and the ensamble mean.
+    """Calculate the mean error between observed values and the ensemble mean.
 
     Parameters
     ----------
@@ -44,19 +44,130 @@ def ens_me(obs, fcst_ens=None, remove_zero=False, remove_neg=False):
     Returns
     -------
     float
-        The mean error between the observed time series data and the ensamble mean.
+        The mean error between the observed time series data and the ensemble mean.
 
     """
     # Treating data
     obs, fcst_ens = treat_data(obs, fcst_ens, remove_neg=remove_neg, remove_zero=remove_zero)
 
     fcst_ens_mean = np.mean(fcst_ens, axis=1)
-    print(fcst_ens_mean)
 
-    return np.mean(fcst_ens_mean - obs)
+    error = fcst_ens_mean - obs
+    return np.mean(error)
 
 
-def crpsHersbach(obs, fcst):
+def ens_mae(obs, fcst_ens=None, remove_zero=False, remove_neg=False):
+    """Calculate the mean absolute error between observed values and the ensemble mean.
+
+    Parameters
+    ----------
+    obs: 1D ndarray
+        Array of observations for each start date.
+
+    fcst_ens: 2D ndarray
+        Array of ensemble forecast of dimension n x M, where n = number of start dates and
+        M = number of ensemble members.
+
+    remove_neg: bool
+        If True, when a negative value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
+    remove_zero: bool
+        If true, when a zero value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
+    Returns
+    -------
+    float
+        The mean absolute error between the observed time series data and the ensemble mean.
+
+    """
+    # Treating data
+    obs, fcst_ens = treat_data(obs, fcst_ens, remove_neg=remove_neg, remove_zero=remove_zero)
+
+    fcst_ens_mean = np.mean(fcst_ens, axis=1)
+
+    error = fcst_ens_mean - obs
+    return np.mean(np.abs(error))
+
+
+def ens_mse(obs, fcst_ens=None, remove_zero=False, remove_neg=False):
+    """Calculate the mean squared error between observed values and the ensemble mean.
+
+    Parameters
+    ----------
+    obs: 1D ndarray
+        Array of observations for each start date.
+
+    fcst_ens: 2D ndarray
+        Array of ensemble forecast of dimension n x M, where n = number of start dates and
+        M = number of ensemble members.
+
+    remove_neg: bool
+        If True, when a negative value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
+    remove_zero: bool
+        If true, when a zero value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
+    Returns
+    -------
+    float
+        The mean error between the observed time series data and the ensemble mean.
+
+    """
+    # Treating data
+    obs, fcst_ens = treat_data(obs, fcst_ens, remove_neg=remove_neg, remove_zero=remove_zero)
+
+    fcst_ens_mean = np.mean(fcst_ens, axis=1)
+
+    error = fcst_ens_mean - obs
+    return np.mean(error**2)
+
+
+def ens_rmse(obs, fcst_ens=None, remove_zero=False, remove_neg=False):
+    """Calculate the root mean squared error between observed values and the ensemble mean.
+
+    Parameters
+    ----------
+    obs: 1D ndarray
+        Array of observations for each start date.
+
+    fcst_ens: 2D ndarray
+        Array of ensemble forecast of dimension n x M, where n = number of start dates and
+        M = number of ensemble members.
+
+    remove_neg: bool
+        If True, when a negative value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
+    remove_zero: bool
+        If true, when a zero value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
+    Returns
+    -------
+    float
+        The mean error between the observed time series data and the ensemble mean.
+
+    """
+    # Treating data
+    obs, fcst_ens = treat_data(obs, fcst_ens, remove_neg=remove_neg, remove_zero=remove_zero)
+
+    fcst_ens_mean = np.mean(fcst_ens, axis=1)
+
+    error = fcst_ens_mean - obs
+    return np.sqrt(np.mean(error**2))
+
+
+def crps_hersbach(obs, fcst_ens, remove_neg=False, remove_zero=False):
     """Calculate the the continuous ranked probability score (CRPS) as per equation 25-27 in
     Hersbach et al. (2000)
 
@@ -64,9 +175,19 @@ def crpsHersbach(obs, fcst):
     ----------
     obs: 1D ndarry
         Array of observations for each start date
-    fcst: 2D ndarray
+    fcst_ens: 2D ndarray
         Array of ensemble forecast of dimension n x M, where n = number of start dates and
         M = number of ensemble members.
+
+    remove_neg: bool
+        If True, when a negative value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
+    remove_zero: bool
+        If true, when a zero value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
 
     Returns
     -------
@@ -78,8 +199,8 @@ def crpsHersbach(obs, fcst):
 
     Notes
     -----
-    **NaN treatment:** If any start date in obs is NaN, then the corresponding row in fcst (for all
-    ensemble members) will also be deleted.
+    **NaN and inf treatment:** If any value in obs or fcst_ens is NaN or inf, then the
+    corresponding row in both fcst_ens (for all ensemble members) and in obs will be deleted.
 
     References
     ----------
@@ -87,26 +208,12 @@ def crpsHersbach(obs, fcst):
       for Ensemble Prediction Systems, Weather and Forecasting, 15, 559-570.
     """
 
-    # Make sure obs and fcst have same number of start dates & correct dims
-    assert obs.ndim == 1, "crpsHersbach() failed as obs not 1d-array!"
-    assert fcst.ndim == 2, "crpsHersbach() failed as fcst not 2d-array!"
-    assert len(obs) == len(fcst[:, 0]), "crpsHersbach() failed due to " \
-                                        "different length start dates " \
-                                        "between obs and fcst!"
-
-    # Give user warning, but let run, if eith obs or fcst are all zeros
-    if obs.sum() == 0 or fcst.sum() == 0:
-        warnings.warn("All zero values in either 'obs' or 'fcst', "
-                      "crpsHersbach() will run, but check if data OK!")
-
-    # Treat missing data in obs, by deleting respective row in fcst, then in obs
-    if np.isnan(obs).any():
-        fcst = np.delete(fcst, np.argwhere(np.isnan(obs)), axis=0)
-        obs = np.delete(obs, np.argwhere(np.isnan(obs)))
+    # Treating the Data
+    obs, fcst_ens = treat_data(obs, fcst_ens, remove_neg=remove_neg, remove_zero=remove_zero)
 
     # Set parameters
-    n = fcst.shape[0]  # number of forecast start dates
-    m = fcst.shape[1]  # number of ensemble members
+    n = fcst_ens.shape[0]  # number of forecast start dates
+    m = fcst_ens.shape[1]  # number of ensemble members
 
     # Create vector of pi's
     p = np.linspace(0, m, m + 1)
@@ -116,8 +223,8 @@ def crpsHersbach(obs, fcst):
     # crpsAdj = np.zeros(n)
 
     # Matrices for alpha and beta in CRPS decomposition
-    aMat = np.zeros(shape=(n, m + 1))
-    bMat = np.zeros(shape=(n, m + 1))
+    a_mat = np.zeros(shape=(n, m + 1))
+    b_mat = np.zeros(shape=(n, m + 1))
 
     # Loop fcst start times
     for i in range(n):
@@ -130,7 +237,7 @@ def crpsHersbach(obs, fcst):
         xa = obs[i]
 
         # Ensemble fcst CDF
-        x = np.sort(fcst[i, :])
+        x = np.sort(fcst_ens[i, :])
 
         # Deal with 0 < i < m [So, will loop 50 times for m = 51]
         for j in range(m - 1):
@@ -141,7 +248,7 @@ def crpsHersbach(obs, fcst):
                 b[j] = 0
 
             # Rule 2
-            if xa > x[j] and xa < x[j + 1]:
+            if x[j] < xa < x[j + 1]:
                 a[j] = xa - x[j]
                 b[j] = x[j + 1] - xa
 
@@ -173,29 +280,29 @@ def crpsHersbach(obs, fcst):
         b = np.insert(b, 0, b1)
         b = np.append(b, bm)
 
-        # Populate aMat and bMat
-        aMat[i, :] = a
-        bMat[i, :] = b
+        # Populate a_mat and b_mat
+        a_mat[i, :] = a
+        b_mat[i, :] = b
 
         # Calc crps for individual start times
         crps[i] = ((a * pi ** 2) + (b * (1 - pi) ** 2)).sum()
 
     # Calc mean crps as simple mean across crps[i]
-    crpsMean_method1 = crps.mean()
+    crps_mean_method1 = crps.mean()
 
     # Calc mean crps across all start times from eqn. 28 in Hersbach (2000)
-    abar = np.mean(aMat, 0)
-    bbar = np.mean(bMat, 0)
-    crpsMean_method2 = ((abar * pi ** 2) + (bbar * (1 - pi) ** 2)).sum()
+    abar = np.mean(a_mat, 0)
+    bbar = np.mean(b_mat, 0)
+    crps_mean_method2 = ((abar * pi ** 2) + (bbar * (1 - pi) ** 2)).sum()
 
     # Output array as a dictionary
-    output = {'crps': crps, 'crpsMean1': crpsMean_method1,
-              'crpsMean2': crpsMean_method2}
+    output = {'crps': crps, 'crpsMean1': crps_mean_method1,
+              'crpsMean2': crps_mean_method2}
 
     return output
 
 
-def crpsKernel(obs, fcst):
+def crps_kernel(obs, fcst_ens, remove_neg=False, remove_zero=False):
     """Compute the kernel representation of the continuous ranked probability score (CRPS).
 
     Calculates the kernel representation of the continuous ranked probability score (CRPS) as per
@@ -209,9 +316,20 @@ def crpsKernel(obs, fcst):
     obs: 1D ndarray
         Arrar of observations for each start date.
 
-    fcst: 2D ndarray
+    fcst_ens: 2D ndarray
         Array of ensemble forecast of dimension n x M, where n = number of start dates and
         M = number of ensemble members.
+
+    remove_neg: bool
+        If True, when a negative value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
+    remove_zero: bool
+        If true, when a zero value is found at the i-th position in the observed OR ensemble
+        array, the i-th value of the observed AND ensemble array are removed before the
+        computation.
+
 
     Returns
     -------
@@ -235,25 +353,12 @@ def crpsKernel(obs, fcst):
       Q. J. R. Meteorol., Accepted.
     """
 
-    # Make sure obs and fcst have same number of start dates & correct dims
-    assert obs.ndim == 1, "crpsKernel() failed as obs not 1D-array!"
-    assert fcst.ndim == 2, "crpsKernel() failed as fcst not 2D-array!"
-    assert len(obs) == len(fcst[:, 0]), "crpsKernel() failed due to different " \
-                                        "length start dates between obs and fcst!"
-
-    # Give user warning, but let run, if eith obs or fcst are all zeros
-    if obs.sum() == 0 or fcst.sum() == 0:
-        warnings.warn("All zero values in either 'obs' or 'fcst', crpskernel() "
-                      "will run, but check if data OK!")
-
-    # Treat missing data in obs, by deleting respective row in fcst, then in obs
-    if np.isnan(obs).any():
-        fcst = np.delete(fcst, np.argwhere(np.isnan(obs)), axis=0)
-        obs = np.delete(obs, np.argwhere(np.isnan(obs)))
+    # Treatment of data
+    obs, fcst_ens = treat_data(obs, fcst_ens, remove_neg=remove_neg, remove_zero=remove_zero)
 
     # Set parameters
-    n = fcst.shape[0]  # number of forecast start dates
-    m = fcst.shape[1]  # number of ensemble members
+    n = fcst_ens.shape[0]  # number of forecast start dates
+    m = fcst_ens.shape[1]  # number of ensemble members
 
     # Initialise vectors for storing output
     t1 = np.zeros(n)
@@ -264,14 +369,14 @@ def crpsKernel(obs, fcst):
     # Loop through start dates
     for i in range(n):
 
-        t1[i] = abs(fcst[i] - obs[i]).sum()
+        t1[i] = abs(fcst_ens[i] - obs[i]).sum()
 
         # Initialise a vec for storing absolute errors for each ensemble pair
         vec = np.zeros(m)
 
         # Loop through ensemble members
         for j in range(m):
-            vec[j] = abs(fcst[i, j] - np.delete(fcst[i, :], j)).sum()
+            vec[j] = abs(fcst_ens[i, j] - np.delete(fcst_ens[i, :], j)).sum()
             # vec[j] = abs(fcst[i, j] - fcst[i, :]).sum()
 
         t2[i] = vec.sum()
@@ -302,22 +407,22 @@ def treat_data(obs, fcst_ens, remove_zero, remove_neg):
     obs: 1D ndarray
         Arrar of observations for each start date.
 
-    fcst: 2D ndarray
+    fcst_ens: 2D ndarray
         Array of ensemble forecast of dimension n x M, where n = number of start dates and
         M = number of ensemble members.
 
     remove_zero: bool
         If True, zeros will be removed at the i-th value from both the observed time series data
-        and the ensamble data at the i-th position.
+        and the ensemble data at the i-th position.
 
     remove_neg: bool
         If True, negative values will be removed at the i-th value from both the observed time
-        series data and the ensamble data at the i-th position.
+        series data and the ensemble data at the i-th position.
 
     Returns
     -------
     tuple of ndarrays
-        Returns the treated observed data and the ensemble data.
+        Returns the treated observed data and the ensemble data in a tuple.
     """
     assert obs.ndim == 1, "obs is not a 1D numpy array."
     assert fcst_ens.ndim == 2, "fcst_ens is not a 2D numpy array."
@@ -338,6 +443,15 @@ def treat_data(obs, fcst_ens, remove_zero, remove_neg):
         fcst_ens = fcst_ens[all_nan_indices, :]
 
         warnings.warn("The observed data contained NaN values and they have been removed.")
+
+    if np.isinf(obs).any() or np.isinf(fcst_ens).any():
+        inf_indices_fcst = ~(np.any(np.isnan(fcst_ens), axis=1))
+        inf_indices_obs = ~np.isnan(obs)
+        all_inf_indices = np.logical_and(inf_indices_fcst, inf_indices_obs)
+        obs = obs[all_inf_indices]
+        fcst_ens = fcst_ens[all_inf_indices, :]
+
+        warnings.warn("The observed data contained inf values and they have been removed.")
 
     # Treat zero data in obs and fcst_ens, rows in fcst_ens or obs that contain zero values
     if remove_zero:
@@ -373,12 +487,12 @@ if __name__ == "__main__":
                    r'Forecast_Skill/West_Rapti_Kusum_River_Discharge_2017-08-05_2017-08-15_' \
                    r'Hourly.csv'
 
-    ensamble_df = pd.read_csv(forecast_URL, index_col=0)
+    ensemble_df = pd.read_csv(forecast_URL, index_col=0)
     hydrologic_df = pd.read_csv(observed_URL, index_col=0)
 
-    # Converting Ensamble DF index to datetime
-    ensamble_df.index = pd.to_datetime(ensamble_df.index)
-    time_values = ensamble_df.index
+    # Converting ensemble DF index to datetime
+    ensemble_df.index = pd.to_datetime(ensemble_df.index)
+    time_values = ensemble_df.index
 
     # Cleaning up the observed_data
     hydrologic_df = hydrologic_df.dropna()
@@ -389,10 +503,10 @@ if __name__ == "__main__":
     hydrologic_df = hydrologic_df.reindex(time_values).dropna()
 
     # Merging the data
-    merged_df = pd.DataFrame.join(hydrologic_df, ensamble_df)
-    # merged_df.to_csv('merged_ensamble_df.csv')
+    merged_df = pd.DataFrame.join(hydrologic_df, ensemble_df)
+    # merged_df.to_csv('merged_ensemble_df.csv')
 
-    obs = merged_df.iloc[:, 0].values
-    fcst_ens = merged_df.iloc[:, 1:].values
+    obs_array = merged_df.iloc[:, 0].values
+    fcst_ens_matrix = merged_df.iloc[:, 1:].values
 
-    print(ens_me(obs, fcst_ens))
+    print(crps_kernel(obs_array, fcst_ens_matrix))
