034730200cc13bd501bf34301e3594fd354c4a35
Wade Roberts <waderoberts123@gmail.com>
2018-08-27 20:16:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     url='https://github.com/waderoberts123/hydrostats',  # use the URL to the github repo
     download_url='https://github.com/waderoberts123/Hydrostats/archive/0.70.tar.gz',
     keywords=['hydrology', 'error', 'metrics', 'comparison', 'statistics', 'forecast', 'observed'],
-    # arbitrary keywords
     classifiers=["License :: OSI Approved :: MIT License",
                  "Programming Language :: Python :: 3.6",
                  ],
